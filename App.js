ea112de287f389202d70b23fc4c2397829ca9e93
nhlamuloj <nmahlabana@gmail.com>
2022-10-07 16:09:32 +0200

    files

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import Register from './src/Register'
 import Login from './src/Login';
 import Pro  from './src/pro';
 import NotFound from './src/NotFound';
+import AddScammer from './src/AddScammer'
 
 
 
@@ -15,7 +16,7 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-            <Stack.Navigator initialRouteName='Home'>
+            <Stack.Navigator initialRouteName='Register'>
             <Stack.Screen name='Register' options={{title:'Register', headerShown:false}}>
                     {(props)=> <Register {...props} />}
                   </Stack.Screen>
@@ -33,6 +34,9 @@ function App() {
                   </Stack.Screen>
                   <Stack.Screen name='NotFound' options={{title:'NotFound', headerShown:false}}>
                     {(props)=> <NotFound {...props} />}
+                  </Stack.Screen>
+                  <Stack.Screen name='AddScammer' options={{title:'AddScammer', headerShown:false}}>
+                    {(props)=> <AddScammer {...props} />}
                   </Stack.Screen>
             </Stack.Navigator>
     </NavigationContainer>
