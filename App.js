8ce73d336161b5625975353e63d2ba086513ec71
TedHopewell <hopewellmaome@gmail.com>
2022-10-12 11:28:02 +0200

    drowndown styling complete and mapping in progresss

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-            <Stack.Navigator initialRouteName='Login'>
+            <Stack.Navigator initialRouteName='Home'>
             <Stack.Screen name='Register' options={{title:'Register', headerShown:false}}>
                     {(props)=> <Register {...props} />}
                   </Stack.Screen>
