64953760d838a05c912e646bb87ea87e61a846f3
tebogo H <47559292+tebo27cMd@users.noreply.github.com>
2022-10-19 12:48:12 +0200

    added add flag

---
+++ .history/components/topPart_20221019095143.js
@@ -0,0 +1,68 @@
+import { StyleSheet, SafeAreaView, Text, View, } from 'react-native';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { Link } from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native-web';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+export default function RedPart({ navigation }) {
+  return (
+
+    <View style={styles.topContainer} >
+      <View style={styles.btnBackground}>
+        <TouchableOpacity 
+       
+        >  
+          <Text style={styles.usernameTXT1}><Link to={'/Login'}>Login</Link></Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          title="register"
+          onPress={() => navigation.push('register')}>
+          <Text style={styles.usernameTXT}>Register</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity >
+        <FontAwesomeIcon icon={faEllipsisV} style={styles.menuDots} />
+      </TouchableOpacity>
+
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  topContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    backgroundColor: '#D2373C',
+    height: '50px',
+    width: '100%',
+    paddingTop: 15,
+    paddingRight: 20,
+    justifyContent: 'flex-end',
+  },
+  btnBackground: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginRight: '205px',
+
+  },
+  usernameTXT1: {
+    paddingRight: 15,
+    marginRight: 5,
+    borderRightWidth: '2px',
+    borderRightColor: 'white',
+    height: 20,
+    color: 'white',
+  },
+  usernameTXT: {
+    textAlign: 'right',
+    paddingLeft: 10,
+    color: 'white',
+  },
+  menuDots: {
+    color: 'white',
+  },
+
+});
---
+++ .history/src/AddScammer_20221019105502.js
@@ -0,0 +1,188 @@
+import { SafeAreaView, StyleSheet, Text, View,TextInput,TouchableOpacity } from 'react-native'
+import React, { useState,useEffect } from 'react'
+import { db } from './config/firebase';
+import {addDoc,collection,getDocs} from 'firebase/firestore'
+import Top from '../components/secureTopParts'
+import Icon  from 'react-native-vector-icons/FontAwesome'
+import DropdownComponent from '../components/dropdownList';
+
+
+const AddScammer = () => {
+    const [address,setAddress]=useState('')
+    // const [type,setType]=useState('')
+    const[items, setItem] = useState([]);
+
+    const itemRef =collection(db,"flags");
+    const getItems = async()=>{
+        let data = await getDocs(itemRef);
+        setItem(data.docs.map((doc)=>({...doc.data(), id: doc.id})))
+    }
+    
+  const addFlag = async()=>{
+    await addDoc(itemRef, {address:address})
+    alert("flag added Successfully");
+   getItems();
+   }
+   useEffect(()=>{
+    getItems();
+ }, [])
+    
+  return (
+    <SafeAreaView style={styles.container}>
+        <Top/>
+        <View style={styles.midContainer}>
+            <View style={styles.text}>
+                <Text style={styles.txt}>ADD A SCAMMER</Text>
+            </View>
+            <View style={styles.hhh}>
+
+            <View style={styles.selectView}>
+            <DropdownComponent/>
+          </View>
+
+
+            <View style={styles.txtInput}>
+            <Icon
+                color='red'
+                name='flag'
+                type='font-awesome'
+                size={15}
+            />
+            <TextInput 
+                placeholder='Enter Scam Address'
+                style={styles.textInput}
+                onChange={(e)=>setAddress(e.target.value)}/>
+            </View>
+            <View style={styles.comment}>
+                <TextInput
+                placeholder='Add Comment...'
+                style={styles.addComment}
+                />
+            </View>
+            </View>
+            <View style={styles.buttonContainer}>
+            <TouchableOpacity
+            onClick={addFlag}
+                style={styles.button}
+            >
+                <Text style={styles.buttonText}>FLAG</Text>
+            </TouchableOpacity>
+        </View>
+
+
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default AddScammer
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#FFFFFF',
+        height:'100%',
+        width:'100%'
+    },
+    midContainer:{
+        flex:12,
+        padding:'20px',
+      
+    
+    },
+
+    text:{
+        height:'5px',
+        alignItems:'center',
+        padding:'35px',
+        boxShadow: '#ababab 0px 6px 9px -3px',
+       
+    },
+
+    selectView: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: '35%',
+        
+      
+    },
+    txt:{
+        fontFamily:'coda',
+        fontSize:'normal',
+        fontWeight:400,
+        fontSize: '20px',
+        color: 'rgba(0, 0, 0, 0.8)',
+    },
+
+    txtInput:{
+        width:'100%',
+        height:44,
+        paddingHorizontal:20,
+        display:'flex',
+        marginTop:'10%',
+        flexDirection:'row',
+        alignItems:'center',
+        borderBottomColor:'grey',
+        borderBottomWidth:1,
+        
+
+    
+        
+    },
+
+    textInput:{
+        width:'100%',
+        paddingLeft:'10px', 
+       
+    },
+
+  
+    addComment:{
+        padding:'20px',
+        height:'100px',
+        boxShadow: '#ababab 0px 6px 9px -3px',
+        marginTop:'10%',
+        borderRadius:'10px',
+        borderColor:'grey'
+        
+
+    },
+
+    buttonContainer:{
+        flex:3,
+        width:'100%',
+        justifyContent:'center',
+        alignItems:'center',
+        marginTop:'10%',
+        paddingHorizontal:50
+      },
+  
+      button:{
+        backgroundColor:'#EA4335',
+        width:'100%',
+        padding:15,
+        borderRadius:'27px',
+        alignItems:'center',
+        fontSize:'bold'
+        
+      },
+  
+      buttonText:{
+        color:'white',
+       fontSize:'bold'
+     },
+  
+     bottomContainer:{
+      backgroundColor:'black',
+      marginTop:'10%',
+    },
+
+    hhh:{
+        paddingHorizontal:'25px',
+        marginTop:'100px'
+    }
+
+
+
+    
+})
