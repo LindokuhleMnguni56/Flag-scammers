db8ba8c833b25d77613098d6bee24f593bbd2d2e
tebogo H <47559292+tebo27cMd@users.noreply.github.com>
2022-10-21 12:55:33 +0200

    adding flag

---
+++ .history/src/AddScammer_20221021105811.js
@@ -0,0 +1,220 @@
+import { SafeAreaView, StyleSheet, Text, View,TextInput,TouchableOpacity } from 'react-native'
+import React, { useState,useEffect } from 'react'
+import { db,auth } from './config/firebase';
+import {addDoc,collection} from 'firebase/firestore'
+import Top from '../components/secureTopParts';
+import RedPart2 from '../components/secureTopParts';
+import Icon  from 'react-native-vector-icons/FontAwesome'
+import { Picker } from 'react-native-web'
+import moment from 'moment/moment';
+
+
+const AddScammer = () => {
+    const moment = require('moment')
+    const time = moment() // moment(new Date()).format("YYYY-MM-DD hh:mm:ss")
+    const timestamp =time.format("YYYY-MM-DD HH:mm:ss")
+
+    const [mail,setMail]=useState('');
+    const [comment,setComment]=useState('');    
+
+    const itemRef =collection(db,"flag");
+    const commentRef = collection(db,"comments"); 
+    const user=auth.currentUser
+
+    const [selectedAddress, setSelectedAddress] = useState('');
+    const [addresses] = useState([
+        'Email Address',
+        'Physical Address'
+    ]
+    );
+    const addflag = async()=>{
+        if (user!=null){
+            //add to flag
+            // const docRef = await addDoc(itemRef,{address:mail ,userId:user.uid,addressType:selectedAddress, date:timestamp,comment:{[uid]:comment  }})
+            // console.log("New Flag  ID: ", docRef.flag.uid);
+            await addDoc(itemRef,{address:mail ,userId:user.uid,addressType:selectedAddress, date:timestamp})
+            await addDoc(commentRef,{address:mail ,userId:user.uid,flagId:flag.id})
+            
+            //add to comments
+              
+        }
+
+        alert('flag added')
+    }
+
+
+    
+  return (
+    <SafeAreaView style={styles.container}>
+        {/* <RedPart2/> */}
+        <View style={styles.midContainer}>
+            <View style={styles.text}>
+                <Text style={styles.txt}>ADD A SCAMMER</Text>
+            </View>
+            <View style={styles.hhh}>
+
+            <View style={styles.selectView}>
+            {/* <DropdownPicker /> */}
+            <Picker
+            style={[styles.dropdownPick, {marginTop: '25px',marginVertical:10,backgroundColor:''}]}
+            selectedValue = {selectedAddress}
+            onValueChange = {( itemVal) => {
+                setSelectedAddress(itemVal); 
+                console.log({selectedAddress});
+                
+            }}
+            >
+            {
+                addresses.map((a) => (
+                    <Picker.Item label={a} value={a} key={a} />
+                ))
+            }
+        </Picker>
+          </View>
+
+
+            <View style={styles.txtInput}>
+            <Icon
+                color='red'
+                name='flag'
+                type='font-awesome'
+                size={15}
+            />
+            <TextInput onChangeText={mail=>setMail(mail)}
+                placeholder='Enter Scam Address'
+                style={styles.textInput}
+              />
+            </View>
+            <View style={styles.comment}>
+                <TextInput onChangeText={comment=>setComment(comment)}
+                placeholder='Add Comment...'
+                style={styles.addComment}
+                />
+            </View>
+            </View>
+            <View style={styles.buttonContainer}>
+            <TouchableOpacity
+            onPress={addflag}
+                style={styles.button}
+            >
+                <Text style={styles.buttonText}>FLAG</Text>
+            </TouchableOpacity>
+        </View>
+
+
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default AddScammer
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#FFFFFF',
+        height:'100%',
+        width:'100%'
+    },
+    midContainer:{
+        flex:12,
+        padding:'20px',
+      
+    
+    },
+
+    text:{
+        height:'5px',
+        alignItems:'center',
+        padding:'35px',
+        boxShadow: '#ababab 0px 6px 9px -3px',
+       
+    },
+
+    selectView: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: '35%',
+        
+      
+    },
+    txt:{
+        fontFamily:'coda',
+        fontSize:'normal',
+        fontWeight:400,
+        fontSize: '20px',
+        color: 'rgba(0, 0, 0, 0.8)',
+    },
+
+    txtInput:{
+        width:'100%',
+        height:44,
+        paddingHorizontal:20,
+        display:'flex',
+        marginTop:'10%',
+        flexDirection:'row',
+        alignItems:'center',
+        borderBottomColor:'grey',
+        borderBottomWidth:1,
+        
+
+    
+        
+    },
+
+    textInput:{
+        width:'100%',
+        paddingLeft:'10px', 
+       
+    },
+
+  
+    addComment:{
+        padding:'20px',
+        height:'100px',
+        boxShadow: '#ababab 0px 6px 9px -3px',
+        marginTop:'10%',
+        borderRadius:'10px',
+        borderColor:'grey'
+        
+
+    },
+
+    buttonContainer:{
+        flex:3,
+        width:'100%',
+        justifyContent:'center',
+        alignItems:'center',
+        marginTop:'10%',
+        paddingHorizontal:50
+      },
+  
+      button:{
+        backgroundColor:'#EA4335',
+        width:'100%',
+        padding:15,
+        borderRadius:'27px',
+        alignItems:'center',
+        fontSize:'bold'
+        
+      },
+  
+      buttonText:{
+        color:'white',
+       fontSize:'bold'
+     },
+  
+     bottomContainer:{
+      backgroundColor:'black',
+      marginTop:'10%',
+    },
+
+    hhh:{
+        paddingHorizontal:'25px',
+        marginTop:'100px'
+    }
+
+
+
+    
+})
---
+++ .history/src/Comments_20221021121658.js
@@ -0,0 +1,199 @@
+import {StyleSheet,SafeAreaView, View,Text, TextInput, ScrollView,Image } from 'react-native';
+import RedPart from '../components/topPart';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faFlag, faPlane } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import profileImg from '../assets/pic.png'
+import { Button, TouchableOpacity } from 'react-native-web';
+import { Icon } from 'react-native-vector-icons/Icon';
+
+export default function Comments(){
+    return(
+        <SafeAreaView style={styles.container}>
+            <RedPart />
+            <View style={styles.card}>
+                    <View style={styles.dateContainer}>
+                        <View style={styles.dateContainerBorder}>
+                            <Text style={styles.day}>15</Text>
+                            <Text style={styles.month}>May</Text>
+                            <Text style={styles.year}>2021</Text>
+                        </View>
+                    </View>
+                    <View style={styles.userContainer}>
+                          <View style={styles.userContainerBorder}>
+                                  <Text style={styles.username}>tebzahamham@gmail.com</Text>
+                          </View>
+                          <View style={styles.likes}>
+                                          <FontAwesomeIcon icon={ faFlag } style={styles.flags}/><Text style={styles.numberFlags}>1k</Text>
+                          </View>
+                    </View>
+                    
+                </View>
+            <ScrollView style={styles.midContainer} showsVerticalScrollIndicator={false}>
+                
+                <View style={styles.textBox} >
+                        <TouchableOpacity style={styles.userIconBg}>
+                            <Image source={profileImg} style={{height:30, borderRadius:50,}} />
+                        </TouchableOpacity>
+                        
+                        <TextInput style={styles.addComment} placeholder='Add a comment....'></TextInput>
+                        <TouchableOpacity><Text</TouchableOpacity>
+                </View>
+                <View style={styles.commentsBox}>
+                        <TouchableOpacity style={styles.userIconBackground}>
+                            <Image source={profileImg} style={{height:30, borderRadius:50,}} />
+                        </TouchableOpacity>
+                        <Text style={styles.userComments}>
+                                I got scammed twice with this email the scammer asked for my ID number and proof of residence
+                                and also R250 fee I only realized after I paid that I just gave away 
+                                my coonfidential information and even paid on top of that.
+                                Thank you for this app I wish I knew it earlier.
+                        </Text>
+                </View>
+                
+                
+            </ScrollView>
+            <View style={styles.bottomContainer}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 120"><path fill="white" fillOpacity="1" d="M0,32L120,53.3C240,75,380,117,720,117.3C960,117,1200,75,1320,53.3L1440,32L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path></svg>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      alignItems:'center',
+      backgroundColor:'white',
+    },
+    midContainer:{
+        flex:12,
+        height:'500px',
+        width:'100%',
+        marginTop:20,
+      },
+    bottomContainer:{
+        backgroundColor:'#000000',
+        height:'17%',
+        width:'100%',
+    },
+
+    card:{
+    display:'flex',
+    flexDirection:'row',
+    height:60,
+    width:'92%',
+    marginTop:20,
+    boxShadow:'#ababab 0px 6px 9px -3px;',
+    },
+    dateContainer:{
+        flex:1,
+        width:80,
+    },
+    dateContainerBorder:{
+      height:40,
+      marginTop:15,
+      borderRightWidth:1,
+      borderRightColor:'black',
+      width:50,
+    },
+    day:{
+        paddingLeft:20,
+        fontSize:10,
+        color:'#D2373C',
+    },
+    month:{
+        paddingLeft:15,
+        fontSize:10,
+        color:'#D2373C',
+        fontWeight:'bolder',
+    },
+    year:{
+        paddingLeft:15,
+        fontSize:10,
+        color:'#D2373C',
+    },
+    userContainer:{
+        flex:3,
+        display:'flex',
+        flexDirection:'row',
+        marginRight:100,
+    },
+    userContainerBorder:{
+        height:40,
+        marginTop:13,
+        paddingTop:10,
+    },
+    likes:{
+        fle:1,
+        display:'flex',
+        flexDirection:'row',
+        height:40,
+        marginLeft:45,
+        marginTop:25,
+    },
+    username:{
+      width:180,
+    },
+    flags:{
+        color:'#D2373C',
+    },
+    numberFlags:{
+      marginLeft:5,
+      fontSize:12,
+    },
+    textBox:{
+        display:'flex',
+        flexDirection:'row',
+        marginLeft:15,
+        marginTop:20,
+        borderBottomWidth:2,
+        borderBottomColor:'#bd5d5d',
+        height:40,
+        width:'92%',
+        
+    },
+    userIconBg:{
+        backgroundColor:'grey',
+        width:30,
+        height:30,
+        borderRadius:50,
+        marginTop:5,
+    },
+    userIcon:{
+        paddingTop:6,
+        paddingLeft:6,
+    },
+    addComment:{
+        marginLeft:15,
+        paddingLeft:5,
+        height:25,
+        width:'100%',
+        marginTop:10,
+    },
+    commentsBox:{
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'space-evenly',
+        marginTop:20,
+    },
+    userIconBackground:{
+        backgroundColor:'grey',
+        width:30,
+        height:30,
+        borderRadius:50,
+        marginTop:5,
+        
+    },
+    commentsUserIcon:{
+        paddingTop:6,
+        paddingLeft:6,
+    },
+    userComments:{
+        fontSize:12,
+        width:'80%',
+        backgroundColor:'rgb(255,240,242)',
+        padding:15,
+    },
+    
+  });
